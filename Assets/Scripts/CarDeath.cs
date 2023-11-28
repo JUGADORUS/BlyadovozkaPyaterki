a270fd1a6995e8da7aa6a6bd5225bfa31e5c8ab2
JUGADORUS <savelilya07@gmail.com>
2023-11-28 15:10:33 +0500

    добавил полицию и поправил камеру

--- Assets/Scripts/CarDeath.cs
+++ Assets/Scripts/CarDeath.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CarDeath : Car
 {
-    [SerializeField] Car car;
     public int Health = 3;
 
     private void OnCollisionEnter(Collision collision)
@@ -21,6 +20,16 @@ public class CarDeath : Car
             if(Health <= 0)
             {
                Destroy(gameObject);
+            }
+        }
+
+        if (collision.gameObject.GetComponent<PoliceChase>())
+        {
+            Health--;
+
+            if (Health <= 0)
+            {
+                Destroy(gameObject);
             }
         }
     }
